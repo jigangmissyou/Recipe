82ce10a770b671494ac7822e4c134b4204d0a574
JigangGuo <707394840@qq.com>
2023-04-09 17:09:30 +1200

    add bottom navigation

--- app/src/main/java/com/example/myapplication/DashboardActivity.java
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -2,10 +2,14 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,21 +22,22 @@ public class DashboardActivity extends AppCompatActivity implements GalleryAdapt
         setContentView(R.layout.activity_dashboard);
         showGallery();
         showContentList();
+        showBottomNav();
     }
 
     @Override
     public void onItemClick(int position) {
-        Log.d("ImageDemo", "Clicked on image " + (position+1));
+        Log.d("ImageDemo", "Clicked on image " + (position + 1));
     }
 
     /**
      * Show a horizontal gallery of images
      */
-    public void showGallery(){
+    public void showGallery() {
         List<Integer> images = Arrays.asList(R.drawable.pic2, R.drawable.pic2, R.drawable.pic2);
         for (int i = 0; i < images.size(); i++) {
             int imageResourceId = images.get(i);
-            Log.d("ImageDemo", "Image " + (i+1) + " resource ID: " + imageResourceId);
+            Log.d("ImageDemo", "Image " + (i + 1) + " resource ID: " + imageResourceId);
         }
         GalleryAdapter adapter = new GalleryAdapter(images, this);
         RecyclerView galleryRecyclerView = findViewById(R.id.galleryRecyclerView);
@@ -44,7 +49,7 @@ public class DashboardActivity extends AppCompatActivity implements GalleryAdapt
     /**
      * Show a vertical list of content items
      */
-    public void showContentList(){
+    public void showContentList() {
         List<ContentItem> contentItems = Arrays.asList(
                 new ContentItem(R.drawable.pic2, "Title 1", "Description 1", true, true),
                 new ContentItem(R.drawable.pic2, "Title 2", "Description 2", true, false),
@@ -52,7 +57,7 @@ public class DashboardActivity extends AppCompatActivity implements GalleryAdapt
                 new ContentItem(R.drawable.pic2, "Title 4", "Description 4", false, false),
                 new ContentItem(R.drawable.pic2, "Title 5", "Description 5", true, true),
                 new ContentItem(R.drawable.pic2, "Title 6", "Description 6", true, false),
-                new ContentItem(R.drawable.pic2, "Title 7", "Description 7",    false, true),
+                new ContentItem(R.drawable.pic2, "Title 7", "Description 7", false, true),
                 new ContentItem(R.drawable.pic2, "Title 8", "Description 8", false, false),
                 new ContentItem(R.drawable.pic2, "Title 9", "Description 9", true, true),
                 new ContentItem(R.drawable.pic2, "Title 10", "Description 10", true, false),
@@ -67,12 +72,37 @@ public class DashboardActivity extends AppCompatActivity implements GalleryAdapt
                 new ContentItem(R.drawable.pic2, "Title 19", "Description 19", false, true),
                 new ContentItem(R.drawable.pic2, "Title 20", "Description 20", false, false),
                 new ContentItem(R.drawable.pic2, "Title 21", "Description 21", true, true)
-                );
+        );
 //
         Adapter adapter2 = new Adapter(this, contentItems);
         RecyclerView contentRecyclerView = findViewById(R.id.content_item_layout);
         RecyclerView.LayoutManager layoutManager2 = new LinearLayoutManager(this);
         contentRecyclerView.setLayoutManager(layoutManager2);
         contentRecyclerView.setAdapter(adapter2);
+    }
+
+    public void showBottomNav() {
+        // show bottom navigation, but don't do anything when a menu item is clicked
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_item_1:
+                        // Handle menu item 1 click
+                        return true;
+                    case R.id.menu_item_2:
+                        // Handle menu item 2 click
+                        return true;
+                    case R.id.menu_item_3:
+                        // Handle menu item 3 click
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+        });
     }
 }
