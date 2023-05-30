0a75db57b771f5e95de93216edae3d92e9165dc2
JigangGuo <707394840@qq.com>
2023-05-31 11:54:58 +1200

    modify

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -24,44 +24,44 @@ public class DetailActivity extends AppCompatActivity {
         // retrieve recipe from intent
         Recipe recipe = (Recipe) intent.getSerializableExtra("item");
         // bind views
-        ImageView bannerImageView = findViewById(R.id.banner_image);
-        TextView titleTextView = findViewById(R.id.recipe_title);
-        TextView descriptionTextView = findViewById(R.id.recipe_desc);
-        TextView nicknameTextView = findViewById(R.id.recipe_author);
-        RecyclerView ingredientRecyclerView = findViewById(R.id.recipe_ingredients_layout);
-        ingredientRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        ImageView bannerImageView = findViewById(R.id.banner_image);
+//        TextView titleTextView = findViewById(R.id.recipe_title);
+//        TextView descriptionTextView = findViewById(R.id.recipe_desc);
+//        TextView nicknameTextView = findViewById(R.id.recipe_author);
+//        RecyclerView ingredientRecyclerView = findViewById(R.id.recipe_ingredients_layout);
+//        ingredientRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         ArrayList<RecipeIngredient> ingredientList = recipe.getIngredients();
         // set adapter for ingredientRecyclerView
         IngredientAdapter ingredientAdapter = new IngredientAdapter(ingredientList);
-        ingredientRecyclerView.setAdapter(ingredientAdapter);
+//        ingredientRecyclerView.setAdapter(ingredientAdapter);
 
 
         RecyclerView stepList = findViewById(R.id.recipe_step_layout);
         stepList.setLayoutManager(new LinearLayoutManager(this));
         Log.d("RecipeStepAdapter", "Recipe Steps: " + recipe.getRecipeSteps().toString());
-        stepList.setAdapter(new RecipeStepAdapter(recipe.getRecipeSteps()));
+        stepList.setAdapter(new RecipeStepAdapter(recipe.getTitle(),recipe.getDescription(), recipe.getNickName(), ingredientList, recipe.getRecipeSteps()));
 //        LinearLayout stepsLayout = findViewById(R.id.recipe_steps);
 //      Glide.with(this).load(recipe.getBannerImageUrl()).into(bannerImageView);
-        titleTextView.setText(recipe.getTitle());
-        descriptionTextView.setText(recipe.getDescription());
-        nicknameTextView.setText("By: "+recipe.getNickName());
+//        titleTextView.setText(recipe.getTitle());
+//        descriptionTextView.setText(recipe.getDescription());
+//        nicknameTextView.setText("By: "+recipe.getNickName());
         // select the first step image as banner image
         if (!recipe.getRecipeSteps().isEmpty()) {
             int lastStepIndex = recipe.getRecipeSteps().size() - 1;
             if (lastStepIndex >= 0) {
                 Step lastStep = recipe.getRecipeSteps().get(lastStepIndex);
                 String lastStepImagePath = lastStep.getImagePath();
-                if (lastStepImagePath != null) {
-                    // Load the last step image into the bannerImageView
-                    Bitmap bitmap = BitmapFactory.decodeFile(lastStepImagePath);
-                    bannerImageView.setImageBitmap(bitmap);
-                }
+//                if (lastStepImagePath != null) {
+//                    // Load the last step image into the bannerImageView
+//                    Bitmap bitmap = BitmapFactory.decodeFile(lastStepImagePath);
+//                    bannerImageView.setImageBitmap(bitmap);
+//                }
             }
         }
         // get recipe.getRecipeSteps and set the first image to bannerImageView
         // show the first step image in bannerImageView
 //        bannerImageView.setImageResource(R.drawable.image2);
-        shareRecipe();
+//        shareRecipe();
         // Abandoned code
 //        for (RecipeStep step : recipe.getRecipeSteps()) {
 //            View stepView = LayoutInflater.from(this).inflate(R.layout.recipe_step, stepsLayout, false);
@@ -76,25 +76,25 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     // share recipe
-    public void shareRecipe() {
-        ImageButton shareButton = findViewById(R.id.share_button);
-        // add listener to share button
-        shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = getIntent();
-                Recipe recipe = (Recipe) intent.getSerializableExtra("item");
-                String shareText = "Check out this recipe: " + recipe.getTitle() + " by " + recipe.getNickName();
-                Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
-                Intent chooser = Intent.createChooser(shareIntent, "Share this recipe");
-                if (shareIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(chooser);
-                }
-            }
-        });
-
-    }
+//    public void shareRecipe() {
+//        ImageButton shareButton = findViewById(R.id.share_button);
+//        // add listener to share button
+//        shareButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = getIntent();
+//                Recipe recipe = (Recipe) intent.getSerializableExtra("item");
+//                String shareText = "Check out this recipe: " + recipe.getTitle() + " by " + recipe.getNickName();
+//                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//                shareIntent.setType("text/plain");
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+//                Intent chooser = Intent.createChooser(shareIntent, "Share this recipe");
+//                if (shareIntent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(chooser);
+//                }
+//            }
+//        });
+//
+//    }
 
 }
--- app/src/main/java/com/example/myapplication/RecipeStepAdapter.java
+++ app/src/main/java/com/example/myapplication/RecipeStepAdapter.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
@@ -6,53 +7,244 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.recyclerview.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-public class RecipeStepAdapter extends RecyclerView.Adapter<RecipeStepViewHolder> {
+import java.io.File;
+
+public class RecipeStepAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_SHARE = 1;
+    private static final int TYPE_IMAGE = 2;
+    private static final int TYPE_TITLE = 3;
+    private static final int TYPE_CONTENT = 4;
+    private static final int TYPE_AUTHOR = 5;
+    private static final int TYPE_INGREDIENT = 6;
+    private static final int TYPE_STEP = 7;
+
+
+    private String title;
+    private String content;
+
+    private  String author;
+
+    private ArrayList<RecipeIngredient> recipeIngredients;
     private List<Step> steps;
-    public RecipeStepAdapter(List<Step> steps) {
+
+    public RecipeStepAdapter(String title, String content, String author, ArrayList<RecipeIngredient> recipeIngredients, List<Step> steps) {
+        this.title = title;
+        this.content = content;
+        this.recipeIngredients = recipeIngredients;
         this.steps = steps;
     }
+
     @Override
-    public RecipeStepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_step, parent, false);
-        return new RecipeStepViewHolder(itemView);
-    }
-    @Override
-    public void onBindViewHolder(RecipeStepViewHolder holder, int position) {
-        for (Step step : steps) {
-            Log.d("RecipeStepAdapter", "Step: " + step.toString());
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_SHARE;
+        } else if (position == 1) {
+            return TYPE_IMAGE;
+        } else if (position == 2) {
+            return TYPE_TITLE;
+        } else if (position == 3) {
+            return TYPE_CONTENT;
+        } else if (position == 4) {
+            return TYPE_AUTHOR;
+        } else if (position == 5 + steps.size()) {
+            return TYPE_INGREDIENT;
+        } else if (position == 6 + steps.size()){
+            return TYPE_STEP;
+        } else {
+            return TYPE_STEP;
         }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView;
+        switch (viewType) {
+            case TYPE_TITLE:
+                itemView = inflater.inflate(R.layout.detail_title, parent, false);
+                return new TitleViewHolder(itemView);
+            case TYPE_CONTENT:
+                itemView = inflater.inflate(R.layout.detail_content, parent, false);
+                return new ContentViewHolder(itemView);
+            case TYPE_IMAGE:
+                itemView = inflater.inflate(R.layout.detail_image, parent, false);
+                return new ImageViewHolder(itemView);
+            case TYPE_STEP:
+                itemView = inflater.inflate(R.layout.recipe_step, parent, false);
+                return new RecipeStepViewHolder(itemView);
+            case TYPE_SHARE:
+                itemView = inflater.inflate(R.layout.detail_toolbar, parent, false);
+                return new ShareViewHolder(itemView);
+            case TYPE_AUTHOR:
+                itemView = inflater.inflate(R.layout.detail_author, parent, false);
+                return new AuthorViewHolder(itemView);
+            case TYPE_INGREDIENT:
+                itemView = inflater.inflate(R.layout.ingredient_item, parent, false);
+                return new IngredientViewHolder(itemView);
+            default:
+                throw new IllegalArgumentException("Invalid view type");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        Log.d("RecipeStepAdapter log is:", "onBindViewHolder: " + viewType);
+        Log.d("RecipeStepAdapter position is:", "onBindViewHolder: " + position);
+        switch (viewType) {
+            case TYPE_TITLE:
+                ((TitleViewHolder) holder).titleTextView.setText(title);
+                break;
+            case TYPE_CONTENT:
+                ((ContentViewHolder) holder).contentTextView.setText(content);
+                break;
+            case TYPE_STEP:
+                int stepPosition = position - 6 - steps.size() - 1;
+                if (stepPosition >= 0 && stepPosition < steps.size()) {
+                    stepView(stepPosition, (RecipeStepViewHolder) holder);
+                }
+                break;
+
+//                stepView(position, (RecipeStepViewHolder) holder);
+//                break;
+            case TYPE_IMAGE:
+                // Handle image layout
+                break;
+            case TYPE_SHARE:
+//                ((ShareViewHolder) holder).shareImageView.setImageResource(R.drawable.ic_menu_share);
+                // Handle share layout
+                break;
+            case TYPE_AUTHOR:
+                // Handle author layout
+                ((AuthorViewHolder) holder).contentTextView.setText("Author: " + author);
+                break;
+            case TYPE_INGREDIENT:
+                int ingredientPosition = position - 7 - steps.size() - 1;
+                if (ingredientPosition >= 0 && ingredientPosition < recipeIngredients.size()) {
+                    ingredientView(ingredientPosition, (IngredientViewHolder) holder);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid view type");
+
+        }
+    }
+
+    public void stepView(int position, RecipeStepViewHolder holder) {
         Step step = steps.get(position);
-        //log the step
         String imagePath = step.getImagePath();
         if (imagePath == null) {
-            // Set a default image or handle the case when the file doesn't exist
             holder.stepImageView.setImageResource(R.drawable.baseline_panorama_24);
-        }else{
-            // log the image path
-            Log.d("RecipeStepAdapter", "imagePath: " + imagePath);
-            // Load the image from the file path
+        } else {
             File imageFile = new File(imagePath);
             if (imageFile.exists()) {
                 Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-                // Set the image bitmap
                 holder.stepImageView.setImageBitmap(bitmap);
             } else {
-                // Set a default image or handle the case when the file doesn't exist
                 holder.stepImageView.setImageResource(R.drawable.image2);
             }
         }
-
-
         String description = step.getDescription();
         holder.stepTextView.setText(description);
     }
 
+    public void ingredientView(int position, IngredientViewHolder holder) {
+        RecipeIngredient recipeIngredient = recipeIngredients.get(position);
+        String ingredientName = recipeIngredient.getIngredientName();
+        String ingredientQuantity = recipeIngredient.getIngredientQuantity();
+        String ingredientUnit = recipeIngredient.getIngredientUnit();
+        holder.ingredientNameTextView.setText(ingredientName);
+        holder.ingredientQuantityTextView.setText(ingredientQuantity + " " + ingredientUnit);
+    }
 
     @Override
     public int getItemCount() {
-        return steps.size();
+        Log.d("record steps.size + ingredients.size", "getItemCount: "+steps.size() + recipeIngredients.size());
+        Log.d("record steps.size", "getItemCount: "+steps.size());
+        Log.d("record ingredients.size", "getItemCount: "+recipeIngredients.size());
+
+
+        return 5 + steps.size() + recipeIngredients.size();
     }
+
+    static class TitleViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+
+        TitleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.recipe_title);
+        }
+    }
+
+    static class ContentViewHolder extends RecyclerView.ViewHolder {
+        TextView contentTextView;
+
+        ContentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            contentTextView = itemView.findViewById(R.id.recipe_desc);
+        }
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.banner_image);
+        }
+    }
+
+    static class RecipeStepViewHolder extends RecyclerView.ViewHolder {
+        ImageView stepImageView;
+        TextView stepTextView;
+
+        RecipeStepViewHolder(@NonNull View itemView) {
+            super(itemView);
+            stepImageView = itemView.findViewById(R.id.step1_image);
+            stepTextView = itemView.findViewById(R.id.step1_text);
+        }
+//        public RecipeStepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_step, parent, false);
+//            return new RecipeStepViewHolder(itemView);
+//        }
+    }
+
+    static class IngredientViewHolder extends RecyclerView.ViewHolder {
+        TextView ingredientNameTextView;
+        TextView ingredientQuantityTextView;
+        TextView ingredientUnitTextView;
+        IngredientViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ingredientNameTextView = itemView.findViewById(R.id.ingredient_name);
+            ingredientQuantityTextView = itemView.findViewById(R.id.ingredient_quantity);
+            ingredientUnitTextView = itemView.findViewById(R.id.ingredient_unit);
+        }
+    }
+
+    static class ShareViewHolder extends RecyclerView.ViewHolder {
+        ImageView shareImageView;
+
+        ShareViewHolder(@NonNull View itemView) {
+            super(itemView);
+            shareImageView = itemView.findViewById(R.id.share_button);
+        }
+    }
+
+    static class AuthorViewHolder extends RecyclerView.ViewHolder {
+        TextView contentTextView;
+        AuthorViewHolder(@NonNull View itemView) {
+            super(itemView);
+            contentTextView = itemView.findViewById(R.id.recipe_author);
+        }
+    }
+
+
+
 }
